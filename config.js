2de615aa383a8c96ec12b3120482a4498b2dc5bf
Sanjoy mahato <124846542+Sanju54653644@users.noreply.github.com>
2023-05-05 09:17:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
+global.owner = ['917047294917']
+global.ownernomer = "917047294917"
+global.premium = ['917047294917']
 global.packname = 'Sticker By'
 global.author = 'Naze Dev'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
